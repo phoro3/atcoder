1ea9815b18209ae5a6211ede713ad9005cff4557
phoro3 <puhro20@gmail.com>
2019-03-03 23:35:45 +0900

    ABC120 B

---
+++ ABC/ABC_120/B_problem.py
@@ -0,0 +1,9 @@
+A, B, K = map(int, input().split())
+
+count = 0
+for i in reversed(range(1, max(A, B) + 1)):
+    if A % i == 0 and B % i == 0:
+        count += 1
+        if count == K:
+            print(i)
+            break
