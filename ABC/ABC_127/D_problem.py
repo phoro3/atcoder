77c4eb2ef8dcf5382c70efa7af35010d18e92a04
phoro3 <puhro20@gmail.com>
2019-05-26 14:22:15 +0900

    ABC127 D AC

--- ABC/ABC_127/D_problem.py
+++ ABC/ABC_127/D_problem.py
@@ -1,12 +1,20 @@
-#TLE
 N, M = map(int, input().split())
 a_list = list(map(int, input().split()))
-written_pairs = [None] * M
 
+written_nums = []
 for i in range(M):
     b, c = map(int, input().split())
-    a_list += [c] * b
+    written_nums.append((c, b))
 
-a_list = sorted(a_list, reverse = True)
+written_nums.sort(reverse = True)
+append_list = []
+for c, b in written_nums:
+    append_list += [c for _ in range(b)]
+
+    if len(append_list) >= N:
+        break
+
+a_list += append_list
+a_list.sort(reverse = True)
 
 print(sum(a_list[:N]))
